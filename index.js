be3dde8e91fff0dd1146b0ab8e1f30aeaa2b2a1a
ronok420 <shahariarronok41@gmail.com>
2023-12-28 19:13:45 +0600

    local server added to mongodb

--- index.js
+++ index.js
@@ -1,81 +1,3 @@
-// const express = require('express')
-// const cors = require('cors');
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const app = express();
-// const port =process.env.PORT   || 5000;
-
-
-
-// app.use(cors());
-// app.use(express.json())
-
-
-
-
-// const uri = "mongodb+srv://shahariarronok41:mmGdJk8PlbeKkOgU@cluster0.bwilrcc.mongodb.net/?retryWrites=true&w=majority";
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     const database = client.db("usersDB");
-//     const user = database.collection("user");
-//     // await startServer();  
-
-
-//     app.get('/users',async(req,res)=>{
-//       const cursor=user.find()
-//       const result=await cursor.toArray();
-//       res.send(result);
-//     })
-
-//     app.post('/user',async(req,res)=>{
-//       const newUser=req.body;
-//       console.log(newUser);
-//       const result = await user.insertOne(newUser);
-//       res.send(result);
-//       console.log(`A document was inserted with the _id: ${result.insertedId}`);
-//     })
-
-
-
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     // await client.close();
-//     startServer();
-
-//   }
-// }
-
-
-
-// run().catch(console.dir);
-
-
-
-
-// app.get('/',(req,res)=>{
-//     res.send('this is for learning simple crud operation')
-// })
-// app.listen(port,()=>{
-//     console.log(`simple crud is running at port ,${port}`)
-// })
-
-
-//first one  then second
 
 
 
@@ -90,8 +12,14 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  'mongodb+srv://shahariarronok41:mmGdJk8PlbeKkOgU@cluster0.bwilrcc.mongodb.net/?retryWrites=true&w=majority';
+// const uri =
+//   'mongodb://localhost:27017';
+  const uri = 'mongodb://127.0.0.1:27017';
+
+
+
+// const uri =
+//   'mongodb+srv://shahariarronok41:mmGdJk8PlbeKkOgU@cluster0.bwilrcc.mongodb.net/?retryWrites=true&w=majority';
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
